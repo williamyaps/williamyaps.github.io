21248fbe0bf20c599892034cf118a26c08ff43f4
William Yap <williamyaps@yahoo.com>
2023-07-08 00:44:13 +0700

    Create whois.sh
    williamyaps give permit use by DNS cl-gl647c041a.gcdn.co ns1.gcorelabs.net 
    ns2.gcdn.services

---
+++ whois.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#A script to gather whois record of a provided domain and parse it "
+#Author: William Yap
+#July 08,2023
+
+#ns1.gcorelabs.net
+#ns2.gcdn.services
+cl-gl647c041a.gcdn.co
